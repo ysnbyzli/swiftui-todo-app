822903d1471080abdda0d72dfc72270ce179e151
Yasin Beyazlı <yasinbeyazli29@gmail.com>
2022-08-13 11:16:47 +0300

    Initial Commit

---
+++ todoApp/todoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  todoAppApp.swift
+//  todoApp
+//
+//  Created by Yasin Beyazlı on 13.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct todoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
