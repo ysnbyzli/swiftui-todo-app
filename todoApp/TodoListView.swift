85c93aa9cce49e8b36eed6a3d8ff7487b77d3511
Yasin Beyazlı <yasinbeyazli29@gmail.com>
2022-08-14 14:22:13 +0300

    fix: changed main view

---
+++ todoApp/TodoListView.swift
@@ -0,0 +1,20 @@
+//
+//  TodoListView.swift
+//  todoApp
+//
+//  Created by Yasin Beyazlı on 13.08.2022.
+//
+
+import SwiftUI
+
+struct TodoListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TodoListView_Previews: PreviewProvider {
+    static var previews: some View {
+        TodoListView()
+    }
+}
--- todoApp/todoAppApp.swift
+++ todoApp/todoAppApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct todoAppApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ListView()
         }
     }
 }
